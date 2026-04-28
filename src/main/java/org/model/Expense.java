ebd2f555d782179b6af86306e77a0e74510dd69c
btraanhuynhtw <btraan.huynh.tw@gmail.com>
2026-04-28 15:15:20 +0700

    implement User and Expense class

--- src/main/java/org/model/Expense.java
+++ src/main/java/org/model/Expense.java
@@ -4,13 +4,29 @@ import java.io.Serializable;
 import java.util.List;
 
 public class Expense implements Serializable {
-    /* @Parameters
-        - int amount
-        - User paidBy
-        - List<User> participants
-        - String category
-    */
-
+private int amount;
+private User paidBy;
+private List <User> participants;
+private String category;
     // TODO: Constructor
+public Expense(int amount, User paidBy, List<User> participants, String category){
+    this.amount = amount;
+    this.paidBy = paidBy;
+    this.participants = participants;
+    this.category = category;
+}
+}
+
     // TODO: Getter functions
+public int getAmount(){
+    return amount;
+}
+public User getPaidBy(){
+    return paidBy;
+}
+public List <User> getParticipants(){
+    return participants;
+}
+public String getCategory(){
+    return category;
 }
