4f1cfb84725b77baefeb7b54bd9cfc953c8751d2
Long <phamlogg@gmail.com->
2026-05-06 11:11:10 +0800

    implement Group class

--- src/main/java/org/model/Group.java
+++ src/main/java/org/model/Group.java
@@ -5,22 +5,35 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Group implements Serializable {
-    /* @Parameters
-        - String name
-        - String date
-        - boolean settled, defaults to false
-        - List<User> members, defaults to an empty list
-        - List<Expense> expenses, defaults to an empty list
-    */
+    private String name;
+    private String date;
+    private boolean settled = false;
+
+    private List<User> members = new ArrayList<>();
+    private List<Expense> expenses = new ArrayList<>();
 
 
-    // TODO: Constructor
-    // consider not including variables that already have a default value
+    public Group(String name, String date, boolean settled, List<User> members, List<Expense> expenses) {
+        this.name = name;
+        this.date = date;
+    }
 
-    // TODO: Getter functions
+    public void addMember(User user) { members.add(user); }
+    public void addExpense(Expense e) { expenses.add(e); }
 
-    // TODO: addMember()
-    // TODO: addExpense()
-    // TODO: setSettled()
+    public String getName() {
+        return name;
+    }
+    public String getDate() {
+        return date;
+    }
+    public List<User> getMembers() {
+        return members;
+    }
+    public List<Expense> getExpenses() {
+        return expenses;
+    }
+    public boolean isSettled() { return settled; }
 
+    public void setSettled(boolean settled) { this.settled = settled; }
 }
