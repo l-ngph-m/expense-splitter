54473a6909f404b0f245886de3ec5292c953c447
Long <phamlogg@gmail.com->
2026-05-12 16:04:13 +0800

    override equals() and hashCode()

--- src/main/java/org/model/User.java
+++ src/main/java/org/model/User.java
@@ -23,4 +23,17 @@ public class User implements Serializable {
     public void addPayment(double amount) {
         totalPaid += amount;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return name.equals(user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode();
+    }
 }
